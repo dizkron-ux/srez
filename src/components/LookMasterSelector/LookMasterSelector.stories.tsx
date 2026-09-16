4f29b72a9cc1bd33495f1a88187200a42adb0f82
dizkron-ux <dizkron@gmail.com>
2026-09-16 07:56:11 +0300

    Add Look master selector Storybook coverage

---
+++ src/components/LookMasterSelector/LookMasterSelector.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LookMasterSelector } from './LookMasterSelector';
+import { MASTERS } from '../../data/masters';
+
+const meta = {
+  title: 'Patterns/Look master selector',
+  component: LookMasterSelector,
+  parameters: { layout: 'centered' },
+} satisfies Meta<typeof LookMasterSelector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <div style={{ width: 'min(390px, calc(100vw - 48px))' }}>
+      <LookMasterSelector masters={MASTERS} onOpenMaster={() => {}} />
+    </div>
+  ),
+};
